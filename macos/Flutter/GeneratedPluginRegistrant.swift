5a6784fe3103ef8437d5a53e2f18cc2184320149
it camel <itcamel@IT04NB.cicnetgrp.net>
2023-02-22 09:46:52 +0700

    update

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,7 +8,7 @@ import Foundation
 import firebase_core
 import firebase_messaging
 import flutter_local_notifications
-import path_provider_macos
+import path_provider_foundation
 import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
